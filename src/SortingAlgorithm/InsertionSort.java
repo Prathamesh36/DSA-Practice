a2d6649640ec0581b759ecc70db9fc02f2091207
Pratham <abc@gmail.com>
2024-03-19 23:22:39 +0530

    first commit

--- src/SortingAlgorithm/InsertionSort.java
+++ src/SortingAlgorithm/InsertionSort.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InsertionSort {
 
     public static void insertionSort(int[] a,int n){
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             int current =a[i];
             int j=i-1;
             while(j>=0 && current <a[j]){
