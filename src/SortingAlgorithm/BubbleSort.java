a97e4e0d8798fe26c641794453f312a692703449
Pratham <abc@gmail.com>
2024-03-19 22:18:54 +0530

    first commit

--- src/SortingAlgorithm/BubbleSort.java
+++ src/SortingAlgorithm/BubbleSort.java
@@ -1,7 +1,43 @@
 package SortingAlgorithm;
 
-public class BubbleSort {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class BubbleSort {
+
+    public static void bubbleSort(int[] a,int n){
+        for(int i=n-1;i>0;i--){
+            int didSwap=0;
+            for (int j = 0; j < i-1; j++) {
+                if(a[j]>a[j+1]){    //if this condition is false for all iteration,
+                                    // this means that numbers are in sorted order
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    didSwap=1;          // 1 is assigns to this variable if numbers are not in sorted order
+                }
+            }
+            if(didSwap==0){
+                break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.print("Enter Array: ");
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+        }
+        bubbleSort(a,n);
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]+" ");
+        }
     }
 }
+//Worst and Average : O(n^2)
+//Best : O(n) , where already there is sorted array/numbers
